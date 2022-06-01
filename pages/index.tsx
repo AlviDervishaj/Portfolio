4c86f23733061f95fc59050fb48f09348c782659
Alvi Dervishaj <alvidervishaj9@gmail.com>
2022-06-01 12:51:48 +0200

    Added image

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,8 @@ const Home: NextPage = () => {
         <meta name="title" content="Alvi Dervishaj Developer Portfolio" />
         <meta name="description" content="Nothing more to say. Visit my page for more information :)" />
         <meta name="og:title" content="Alvi Dervishaj Portfolio" />
+        <meta name="og:image" content="static/images/tv-app.png" />
+        <meta name="og:image:alt" content="Banner for portfolio-alvi.vercel.app " />
         <meta name="og:description" content="Alvi's Developer Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="og:type" content="website" />
