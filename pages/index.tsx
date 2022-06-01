873c3d2b7abf8d0b8474cc9e05da3c4c16c5d65b
Alvi Dervishaj <alvidervishaj9@gmail.com>
2022-06-01 13:58:40 +0200

    Changed svg to png image

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,20 +13,20 @@ const Home: NextPage = () => {
         <title>Alvi&apos;s Portfolio</title>
         <title>Alvi Dervishaj Portfolio</title>
         <meta name="title" content="Alvi Dervishaj Portfolio" />
-        <meta name="description" content="Nothing more to say. Visit my page for more information :)" />
+        <meta name="description" content="Hello ! I am Alvi Dervishaj, a frontend developer and this is my portfolio." />
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://portfolio-alvi.vercel.app/" />
         <meta property="og:title" content="Alvi Dervishaj Portfolio" />
         <meta property="og:description" content="Hello ! I am Alvi Dervishaj, a frontend developer and this is my portfolio." />
-        <meta property="og:image" content="https://portfolio-alvi.vercel.app/static/images/high.svg" />
+        <meta property="og:image" content="https://portfolio-alvi.vercel.app/static/images/high.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://portfolio-alvi.vercel.app/" />
         <meta property="twitter:creator" content="@alvi_d1" />
         <meta property="twitter:title" content="Alvi Dervishaj Portfolio" />
         <meta property="twitter:description" content="Hello ! I am Alvi Dervishaj, a frontend developer and this is my portfolio." />
-        <meta property="twitter:image" content="https://portfolio-alvi.vercel.app/static/images/high.svg" />
+        <meta property="twitter:image" content="https://portfolio-alvi.vercel.app/static/images/high.png" />
       </Head>
       <Header />
       <About />
